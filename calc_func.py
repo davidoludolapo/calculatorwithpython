72bad54c0cb52fd5cf9e6284e148edc0444976f9
unknown <davidoluwadolapo@gmail.com>
2023-10-18 21:53:47 +0100

    basic calculator with conditions

---
+++ calc_func.py
@@ -0,0 +1,36 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+
+print("""
+Choose an operation from the list:
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Exponentiation
+5. Division
+6. Division with remainder
+""")
+
+op = int(input("Select operation e.g ~2~ "))
+
+if op == 1:
+    print("Sum: {} + {} = {}".format(a, b, a+b))
+elif op ==2: 
+    print("Difference: {} - {} = {}".format(a, b, a+b))
+elif op == 3:
+    print("Product: {} * {} = {}".format(a,b, a*b))
+elif op == 4:
+        print("Power: {}^{} = {}".format(a,b,a))
+elif op == 5:
+        try:
+            print("Quotient: {} / {} = {}".format(a,b,a/b))
+        except:
+            print("Division by 0 not possible!")
+elif op == 6:
+        try:
+            print("Division with remainder: {} / {} = {} Remainder: {}".format(a,b,a//b,a%b))
+        except:
+            print("Divsion by 0 not possible!")
+else:
+        print("No such choice!")
